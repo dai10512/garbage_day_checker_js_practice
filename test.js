824f88fb6f84ec5bde7c7a94731121ad23022286
Daisuke Osanai <dsk.osanai@gmail.com>
2021-10-20 07:46:06 +0900

    deleted some code

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
 console.log('Hello World');
-// alert('This is test');
-// alert("you can cheack today's date");
-
-
-
-// function todaysdate() {
-//   var today = new Date();
-//   console.log(today);
-//   alert(today);
-// }
