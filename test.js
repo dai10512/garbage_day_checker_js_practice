99e8b0257060e87e4d882b6c692777bd4e5a7141
Daisuke Osanai <dsk.osanai@gmail.com>
2021-10-20 07:51:45 +0900

    add code again for safari test

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 console.log('Hello World');
-// alert('This is test');
-// alert("you can cheack today's date");
+alert('This is test');
+alert("you can cheack today's date");
 
 
 
-// function todaysdate() {
-//   var today = new Date();
-//   console.log(today);
-//   alert(today);
-// }
+function todaysdate() {
+  var today = new Date();
+  console.log(today);
+  alert(today);
+}
