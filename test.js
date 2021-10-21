0d8d98d783d1161b397dc098807b93979e2f36e1
Daisuke Osanai <dsk.osanai@gmail.com>
2021-10-21 11:15:55 +0900

    made date format to Japanese style.

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-console.log("Hello World");
-alert('This is test');
-alert("you can cheack today's date");
+var date = new Date();         // 現在日時を生成
+var yyyy = date.getFullYear(); // 西暦を取得
+var mm = date.getMonth() + 1;  // 月を取得（返り値は実際の月-1なので、+1する）
+var dd = date.getDate(); // 日を取得
+var w = date.getDay();   // 曜日を取得（数値）
+ 
+// 月と日が一桁の場合は先頭に0をつける
+// if (mm < 10) {
+//     mm = "0" + mm;
+// }
+// if (dd < 10) {
+//     dd = "0" + dd;
+// }
+ 
+// 曜日を数値から文字列に変換するための配列
+week = ["日", "月", "火", "水", "木", "金", "土"];  
+ 
+var result = yyyy + "年" + mm + "月" + dd + "日" + "(" + week[w] + ")"; // フォーマットを整えて表示
 
-
-
-function todaysdate() {
-  let today = new Date();
-  document.getElementById("dateshow").innerHTML = today
+function todaysDate(){
+document.getElementById("dateshow").innerHTML = result
 }
